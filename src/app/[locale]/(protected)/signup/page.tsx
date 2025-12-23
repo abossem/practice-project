29c9774be17e0a601727c4f119b20284a32ad8f6
abdelrahman assem <abdelrahmanassem222@gmail.com>
2025-12-23 06:50:38 +0200

    remove:(old code to start a new ecom project)

--- src/app/[locale]/(protected)/signup/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>SIGN UP PAGE</div>;
-};
-
-export default page;
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const page = async () => {
+  return <>E com website</>;
 };
 
 export default page;
