1f314de69650c42d03514b455271d0e62767008c
abdelrahman assem <abdelrahmanassem222@gmail.com>
2025-12-29 07:33:12 +0200

    modify:(adTape responsive)

--- src/components/modules/home/AdTape.tsx
+++ src/components/modules/home/AdTape.tsx
@@ -7,7 +7,7 @@ export const AdTape = () => {
   const t = useTranslations("home");
 
   return (
-    <p className="my-5 mx-[30px] rounded-lg bg-primary-50 flex items-center justify-center py-3 leading-[150%] text-lg text-white">
+    <p className="my-5 mx-[30px] rounded-lg bg-primary-50 flex items-center justify-center py-2.5 lg:py-3 leading-[150%] text-sm lg:text-lg text-white">
       {t("adTape")}
       <span className="mx-6 ">
         <ArrowRight size={24} />
